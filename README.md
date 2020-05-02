26f54ec7b4b68989d8998445a58819b8be04429f
Leonardo Marques <leomarques301@gmail.com>
2020-05-02 20:58:02 -0300

    criação do app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# lista-de-tarefas-js
+# Lista de tarefas - JS
 Uma lista de tarefas feita em Javascript puro.
+
+## Screenshot
+<img src="https://imgur.com/c0o97gN.png" width="450">
+
+## Método de armazenamento - Local Storage
+Como não estou utilizando banco de dados nesse projeto, resolvi armazenar os dados das tarefas no local storage do próprio navegador. Cada tarefa tem seu respectivo ID gerado por um script, e pode ser excluida. 
+
+<img src="https://imgur.com/hW3ClmC.png" width="450">
---
+++ app.js
@@ -0,0 +1,74 @@
+
+for (var i = 0; i < window.localStorage.length; i++) {
+  texto = window.localStorage.getItem(window.localStorage.key(i));
+
+    let key = window.localStorage.key(i);
+    let lista = document.getElementById('lista');
+    let div = document.createElement('div');
+    let button = document.createElement('i');
+
+    div.className = 'lista-tarefas tarefa-incompleta';
+    div.innerHTML = texto;
+    button.className = 'fas fa-times';
+    button.id = key;
+    div.id = `${key}-lista`;
+
+    lista.appendChild(div);
+    div.appendChild(button);
+
+    botaoDeletar(document.getElementById(key));
+    completarTarefa(lista = document.getElementById(`${key}-lista`)) 
+};
+
+function criarTarefa(texto = document.getElementById('tarefa-input')) {
+
+  function tokenGenerator(){
+    return Math.random().toString(36).substr(2); 
+  };
+
+  token = tokenGenerator();
+
+  if(texto.value != '') {
+    let lista = document.getElementById('lista');
+    let div = document.createElement('div');
+    let i = document.createElement('i');
+
+    window.localStorage.setItem(token, texto.value);
+    texto2 = window.localStorage.getItem(token);
+    div.className = 'lista-tarefas tarefa-incompleta';
+    div.innerHTML = texto2;
+    i.className = 'fas fa-times';
+    i.id = token;
+    div.id = `${token}-lista`;
+
+    lista.appendChild(div);
+    div.appendChild(i);
+    
+    texto.value = '';
+  };
+  console.log(token);
+};
+
+function botaoDeletar(i = document.getElementById(token)) {
+
+
+  i.addEventListener("click", function(e) {
+    e.stopPropagation();
+    window.localStorage.removeItem(i.id);
+    i.parentNode.remove();
+  });
+
+};
+
+function completarTarefa(lista = document.getElementById(`${token}-lista`)) {
+  lista.addEventListener("click", function(e) {
+    lista.className = 'lista-tarefas tarefa-concluida';
+  });
+};
+
+
+
+
+
+
+
